a3c002f643d6a9f72f4688d095813799d77fa3c9
Morgan A Ray <andreeray@live.com>
2025-01-12 00:40:36 +0100

    Update at 2025-01-11T23:40:36.883Z

--- src/main/windows/clock/clock.js
+++ src/main/windows/clock/clock.js
@@ -22,7 +22,7 @@ function createClockWindow(taskbarWindow) {
 
     clockWindow.loadFile(path.join(__dirname, "../../../renderer/clock/clock.html"));
 
-    clockWindow.webContents.openDevTools({ mode: "detach" }); // Opens DevTools in a separate window
+    // clockWindow.webContents.openDevTools({ mode: "detach" }); // Opens DevTools in a separate window
 
 
     // Dynamically position the clock window relative to the taskbar
