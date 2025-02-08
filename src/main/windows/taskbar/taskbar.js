8ad5002285ba23edec2d7890f4658d76e8845919
Morgan A Ray <andreeray@live.com>
2025-02-08 21:48:54 +0100

    Update at 2025-02-08T20:48:54.659Z

--- src/main/windows/taskbar/taskbar.js
+++ src/main/windows/taskbar/taskbar.js
@@ -4,7 +4,7 @@ const path = require("path");
 function createTaskbarWindow(toggleReminder, toggleSettings) {
     const taskbarWindow = new BrowserWindow({
         width: 50, // Initial size
-        height: 50,
+        height: 60,
         frame: false,
         alwaysOnTop: true,
         transparent: true,
@@ -19,7 +19,7 @@ function createTaskbarWindow(toggleReminder, toggleSettings) {
 
     taskbarWindow.loadFile(path.join(__dirname, "../../../renderer/taskbar/taskbar.html"));
 
-    taskbarWindow.webContents.openDevTools({ mode: "detach" });
+    // taskbarWindow.webContents.openDevTools({ mode: "detach" });
 
     // IPC Listener for resizing the taskbar
     ipcMain.on("resize-taskbar", (event, width, height) => {
