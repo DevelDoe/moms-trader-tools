1fd30968ccf684b43bc94cfb67fffe01a306e21a
Morgan A Ray <andreeray@live.com>
2025-02-08 13:26:26 +0100

    m

--- src/renderer/common/preload.js
+++ src/renderer/common/preload.js
@@ -1,6 +1,9 @@
 const { contextBridge, ipcRenderer, desktopCapturer } = require("electron");
+const createLogger = require("../../hlps/logger");
 
-console.log("Preload script loaded");
+const log = createLogger(__filename); 
+
+log.log("Preload script loaded");
 
 contextBridge.exposeInMainWorld("electronAPI", {
     send: (channel, data) => {
@@ -80,7 +83,7 @@ contextBridge.exposeInMainWorld("regionAPI", {
     send: (channel, data) => ipcRenderer.send(channel, data),
     getSelectedScreen: async () => {
         const selectedScreen = await ipcRenderer.invoke("get-selected-screen");
-        console.log("📡 Selected screen:", selectedScreen);
+        log.log("Selected screen:", selectedScreen);
         return selectedScreen;
     }
 });
