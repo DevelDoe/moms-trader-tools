dc3e1c21f37329895bb69e8343c268c0dfc2d99a
Morgan A Ray <andreeray@live.com>
2025-02-15 00:47:17 +0100

    Update at 2025-02-14T23:47:17.104Z

--- src/main/windows/reminder/reminder.js
+++ src/main/windows/reminder/reminder.js
@@ -11,7 +11,7 @@ function createReminderWindow(taskbarWindow, transparent = true) {
     reminderWindow = new BrowserWindow({
         width: 10,
         height: 10,
-        transparent: true, // ✅ Set based on user settings
+        transparent, // ✅ Set based on user settings
         frame: false,
         resizable: true,
         alwaysOnTop: true,
