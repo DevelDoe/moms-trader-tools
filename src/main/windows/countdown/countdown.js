2ffc9f34a67e8b4a7fb6d82c277886f4c3e798c6
Morgan A Ray <andreeray@live.com>
2025-02-16 14:31:51 +0100

    Update at 2025-02-16T13:31:51.148Z

--- src/main/windows/countdown/countdown.js
+++ src/main/windows/countdown/countdown.js
@@ -9,7 +9,7 @@ function createCountdownWindow(taskbarWindow) {
         height: 50,
         transparent: true,
         frame: false,
-        resizable: true,
+        resizable: false,
         show: false,
         alwaysOnTop: true,
         webPreferences: {
